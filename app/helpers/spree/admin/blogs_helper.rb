0c1f7b98b73a5d53ae3b69aa17c10c7d4053bc07
Matthew Kennedy <m.kennedy@me.com>
2020-11-08 20:29:13 +0000

    update path

--- app/helpers/spree/admin/blogs_helper.rb
+++ app/helpers/spree/admin/blogs_helper.rb
@@ -4,7 +4,12 @@ module Spree
   module Admin
     module BlogsHelper
       def view_in_store_link(blog, store)
-        link_to(store.unique_name, store.url + blog_url(blog), class: "dropdown-item py-2 px-3")
+
+        unless store.url[/\Ahttp:\/\//] || store.url[/\Ahttps:\/\//]
+          store_url = "http://#{store.url}"
+        end
+
+        link_to(store.unique_name, store_url + blog_path(blog), class: "dropdown-item py-2 px-3")
       end
     end
   end
