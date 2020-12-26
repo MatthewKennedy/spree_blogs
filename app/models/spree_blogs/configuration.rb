e10bfc370f71f574639f47b909248397d5d0d7b9
Matthew Kennedy <m.kennedy@me.com>
2020-12-26 15:26:20 +0000

    Update

--- app/models/spree_blogs/configuration.rb
+++ app/models/spree_blogs/configuration.rb
@@ -1,5 +1,7 @@
 module SpreeBlogs
   class Configuration < Spree::Preferences::Configuration
     preference :use_action_text, :boolean, default: false
+    preference :use_raw_post_content, :boolean, default: false
+    preference :image_ratio, :string, default: "1/1"
   end
 end
