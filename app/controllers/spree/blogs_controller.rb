5acae4ca9e6f7a0f2819231cf4dd40cec1adc288
Matthew Kennedy <m.kennedy@me.com>
2021-01-30 18:14:56 +0000

    Update

--- app/controllers/spree/blogs_controller.rb
+++ app/controllers/spree/blogs_controller.rb
@@ -4,7 +4,7 @@ class Spree::BlogsController < Spree::StoreController
   before_action :init_pagination, only: %i[show tag]
 
   def show
-    @blog = Spree::Blog.by_store(current_store).friendly.find(params[:id])
+    set_blog(:id)
 
     @posts = @blog.posts.published_and_visible.page(@pagination_page).per(@pagination_per_page)
 
@@ -16,7 +16,7 @@ class Spree::BlogsController < Spree::StoreController
   end
 
   def tag
-    @blog = Spree::Blog.by_store(current_store).friendly.find(params[:blog_id])
+    set_blog(:blog_id)
 
     @tag = MakeTaggable::Tag.friendly.find(params[:tag])
 
@@ -26,6 +26,10 @@ class Spree::BlogsController < Spree::StoreController
   end
 
   private
+
+  def set_blog(arg)
+    @blog = Spree::Blog.by_store(current_store).friendly.find(params[arg])
+  end
 
   def init_pagination
     @pagination_page = params[:page].present? ? params[:page].to_i : 1
